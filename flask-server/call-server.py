c1fe6a44bd4affc18852327624f04d5672cfd653
subokita <saburo.okita@gmail.com>
2017-04-20 19:06:04 +0200

    Updated call-server to list out the consul services

--- flask-server/call-server.py
+++ flask-server/call-server.py
@@ -7,14 +7,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def call():
-    return "Call"
-
-if __name__ == '__main__':
     consul_agent_address = os.environ.get( 'CONSUL_AGENT_ADDR', 'consul-bootstrap' )
     consul_agent_port    = int(os.environ.get( 'CONSUL_AGENT_PORT', 8500 ))
     consul_client        = consul.Consul( host=consul_agent_address, port=consul_agent_port )
 
+    message = ""
     services = consul_client.agent.services()
     for service in services:
-        print( service )
+        message += str( service ) + "\n"
+
+    return "Call " + message
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
