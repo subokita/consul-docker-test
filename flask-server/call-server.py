5d50835bc94a2485725a8a9e298387e17c6e0542
Saburo Okita <so@re-flekt.com>
2017-04-20 15:44:14 +0200

    Updated to have call server tries to connect to consul server

--- flask-server/call-server.py
+++ flask-server/call-server.py
@@ -1,4 +1,6 @@
 import os
+import uuid
+import consul
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,4 +10,11 @@ def call():
     return "Call"
 
 if __name__ == '__main__':
+    consul_agent_address = os.environ.get( 'CONSUL_AGENT_ADDR', 'consul-bootstrap' )
+    consul_agent_port    = int(os.environ.get( 'CONSUL_AGENT_PORT', 8500 ))
+    consul_client        = consul.Consul( host=consul_agent_address, port=consul_agent_port )
+
+    services = consul_client.agent.services()
+    for service in services:
+        print( service )
     app.run(host='0.0.0.0', port=80, debug=True)
