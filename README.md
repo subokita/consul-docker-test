e340606dba3d3f820bb5c8505a62d97704c19ba0
Saburo Okita <so@re-flekt.com>
2017-04-21 07:49:13 +0200

    - Updated instructions - Used call-server to discover hello-server and REST request it - Updated docker-compose to add more info

--- README.md
+++ README.md
@@ -28,3 +28,22 @@ curl $(docker-machine ip):8500/v1/catalog/services | json_pp
 # Check for the specific service
 curl $(docker-machine ip):8500/v1/catalog/service/hello-server | json_pp
 ```
+
+
+Then finally you could do a proof of concept, using call-server to call hello-server internally
+using Consul's discovery service
+
+```
+# Run curl onto call-server service in consul, and use jq to obtain the port number
+curl -s $(docker-machine ip):8500/v1/catalog/service/call-server | jq -r '.[].ServicePort'
+
+# for example it returns me
+# 32777
+# Now we could call call-server at port 32777
+
+curl -s $(docker-machine ip):32777
+
+# And it should print out something similar to below
+{"CreateIndex": 384, "ServicePort": 32776, "ServiceTags": [], "ModifyIndex": 384, "ServiceEnableTagOverride": false, "Node": "bootstrap-node", "ID": "102b597f-0b4c-4702-985e-327a5a022cc7", "NodeMeta": {}, "TaggedAddresses": {"wan": "192.168.99.100", "lan": "192.168.99.100"}, "Address": "192.168.99.100", "ServiceName": "hello-server", "ServiceID": "ab1720162e4c:hello-server:80", "ServiceAddress": "192.168.99.100"} Hello World
+
+```
--- flask-server/call-server.py
+++ flask-server/call-server.py
@@ -1,22 +1,27 @@
 import os
 import uuid
 import consul
+import requests
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def call():
+    # Create a new consul client, with consul address and port obtained from docker environment variables
     consul_agent_address = os.environ.get( 'CONSUL_AGENT_ADDR', 'consul-bootstrap' )
     consul_agent_port    = int(os.environ.get( 'CONSUL_AGENT_PORT', 8500 ))
     consul_client        = consul.Consul( host=consul_agent_address, port=consul_agent_port )
 
-    message = ""
-    services = consul_client.agent.services()
-    for service in services:
-        message += str( service ) + "\n"
+    # Get the registered hello-server service
+    hello_service = consul_client.catalog.service('hello-server')[1][0]
+    message = json.dumps(hello_service)
 
-    return "Call " + message
+    # Craft the request URL using registered hello-server's address and port
+    url = 'http://%s:%d' % ( hello_service['ServiceAddress'], hello_service['ServicePort'] )
+
+    return message + " " + requests.get( url ).text
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
