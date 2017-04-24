35e4c58d4b567ee7ccc52d6d7a5494d5709611ff
Saburo Okita <so@re-flekt.com>
2017-04-24 15:34:55 +0200

    Updated

--- flask-server/hello-server.py
+++ flask-server/hello-server.py
@@ -1,11 +1,20 @@
 import os
+import requests
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World"
+    consul_agent_address = os.environ.get( 'CONSUL_AGENT_ADDR', 'consul-bootstrap' )
+    consul_agent_port    = int(os.environ.get( 'CONSUL_AGENT_PORT', 8500 ))
+    
+    # consul_agent_address = 'consul-8500.service.consul'
+    # curl 192.168.99.100:8500/v1/catalog/servic
+    url      = ('http://%s:%d/v1/catalog/services') % (consul_agent_address, consul_agent_port)
+    response = requests.get(url).text
+
+    return "Hello World\n" + response 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-docker-machine create --driver virtualbox default
-docker-machine start default
+set -v
+
+docker-machine create --driver virtualbox default || true
+docker-machine start default || true
 eval $(docker-machine env default)
 export DOCKER_MACHINE_IP=$(docker-machine ip default)
+
+set +v
